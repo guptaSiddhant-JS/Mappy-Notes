1d602ff2e7fefeee6f12b564b3ce5ada9970d14b
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-08-01 14:50:22 +0530

    Added leaflet

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ navigator.geolocation.getCurrentPosition(
         const {latitude} = pos.coords;
         const {longitude} = pos.coords;
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        const coords = [latitude , longitude];
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+           }).addTo(map);
+
+        L.marker(coords).addTo(map)
+        .bindPopup('Siddhant Home.')
+        .openPopup();
     },
     () => console.log('Could not get your position')
 )
