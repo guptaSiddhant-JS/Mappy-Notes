1b6cf088ec822390a1ef0057d49681d57149bdc6
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-08-02 10:51:36 +0530

    Added App Architecture

--- script.js
+++ script.js
@@ -5,47 +5,61 @@ const containerLocation = document.querySelector('.location');
 const inputType = document.querySelector('.form__input--type');
 const inputTitle = document.querySelector('.form__input--title');
 const inputNote = document.querySelector('.form__input--note');
-let map , mapEvent;
-// Geoloction 
-if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(
-    pos =>{
-        const {latitude} = pos.coords;
-        const {longitude} = pos.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-        const coords = [latitude , longitude];
-         map = L.map('map').setView(coords, 13);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-           }).addTo(map);
+// App Architecture
+class App {
+    #map;
+    #mapEvent;
+    constructor(){
+       this._getPosition();
 
-        
-        map.on('click' , (mapEl) =>{
-           mapEvent = mapEl;
-           form.classList.remove('hidden');
-           inputTitle.focus();
-     
-        })
-    },
-    () => alert('Could not get your position')
-)
-form.addEventListener('submit' , (el) =>{
-    el.preventDefault();
-    // Clearing the responces
-    inputType.value = inputTitle.value = inputNote.value ='';
-           console.log(mapEvent);
-            const {lat , lng} = mapEvent.latlng;
-            L.marker([lat , lng]).addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth:250,
-                minWidth:100,
-                autoClose:false,
-                closeOnClick:false,
-            })
-        )
-        .setPopupContent('Title')
-        .openPopup();
+       form.addEventListener('submit' , this._newLocation.bind(this));
+    }
+    _getPosition(){
+        // Geoloction 
+        if(navigator.geolocation)
+        navigator.geolocation.getCurrentPosition( this._loadMap.bind(this) , 
+            () => alert('Could not get your position')
+            )
+    }
+    _loadMap(pos){
+            const {latitude} = pos.coords;
+            const {longitude} = pos.coords;
+            // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+            const coords = [latitude , longitude];
+             this.#map = L.map('map').setView(coords, 13);
+    
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+               }).addTo(this.#map);
+    
+            
+            this.#map.on('click' ,this._showForm.bind(this));
+      
+    }
+    _showForm(mapEl){
+        this.#mapEvent = mapEl;
+               form.classList.remove('hidden');
+               inputTitle.focus();
+    }
+    _newLocation(el){
+        el.preventDefault();
+        // Clearing the responces
+        inputType.value = inputTitle.value = inputNote.value ='';
+                const {lat , lng} = this.#mapEvent.latlng;
+                L.marker([lat , lng]).addTo(this.#map)
+            .bindPopup(
+                L.popup({
+                    maxWidth:250,
+                    minWidth:100,
+                    autoClose:false,
+                    closeOnClick:false,
+                })
+            )
+            .setPopupContent('Title')
+            .openPopup();}
 
-})
+
+}
+const app = new App ();
+
